8b019d047c221f60b33d0cc9962d53214847dff3
Nishanthan Ravichandran <nishanthravi226@gmail.com>
2023-02-21 21:50:00 +0530

    LoginActivity added

--- app/src/main/java/nsquare/practice/app/MainActivity.java
+++ app/src/main/java/nsquare/practice/app/MainActivity.java
@@ -2,30 +2,47 @@ package nsquare.practice.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import nsquare.practice.app.constants.Constants;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button homeButton=findViewById(R.id.home_click_button);
+
+
+    Button homeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Constants.APP_NAME="PracticeOnes";
+        initialize();
+    }
 
+    private void initialize() {
+        Constants.APP_NAME="PracticeOnes";
+        homeButton= findViewById(R.id.home_click_button);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Toast.makeText(this,"Welcome to "+Constants.APP_NAME,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                homeButton.setText("Welcome to "+ Constants.APP_NAME);
+
+                Intent i = new Intent(MainActivity.this,LoginPage.class);
+                startActivity(i);
             }
         });
     }
